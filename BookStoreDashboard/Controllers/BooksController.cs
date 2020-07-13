313072b2889105dd5d2b0a4bf4d6783ac8cc766f
Mihail Karlovski <mihailkarlovski@gmail.com>
2020-07-13 21:09:56 +0200

    initial

---
+++ BookStoreDashboard/Controllers/BooksController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BookStoreDashboard.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BookStoreDashboard.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        public async Task<IActionResult> Overview()
+        {
+
+            //http request od C#
+
+            HttpClient httpClient = new HttpClient();
+            var httpResponse = await httpClient.GetAsync("https://localhost:44319/api/books");
+
+            if (httpResponse.IsSuccessStatusCode)
+
+            {
+                var response = await httpResponse.Content.ReadAsStringAsync(); //go vrakja JSON vo string
+
+                var books = JsonConvert.DeserializeObject<List<BookViewModel>>(response);
+
+                return View(books);
+            }
+
+
+            return RedirectToAction("Error", "Home");
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var httpResponse = await httpClient.GetAsync($"https://localhost:44319/api/books/{id}");
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var response = await httpResponse.Content.ReadAsStringAsync(); //go vrakja JSON vo string
+
+                var book = JsonConvert.DeserializeObject<BookViewModel>(response);
+
+                return View(book);
+            }
+
+
+            return RedirectToAction("Error", "Home");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(BookViewModel book)
+        {
+            HttpClient httpClient = new HttpClient();
+            var httpResponse = await httpClient.PutAsJsonAsync("https://localhost:44319/api/books",book);   //http PUT
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                ViewBag.SuccessMessage = "Updated successfully";
+
+                return View();
+            }
+            else
+            {
+                ViewBag.ErrorMessage = "Something went wrong";
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            HttpClient httpClient = new HttpClient();
+            var httpResponse = await httpClient.DeleteAsync($"https://localhost:44319/api/books{id}");
+
+            return RedirectToAction(nameof(Overview));  //vaka ako slucajno go smenis endpointot ke se buni
+        }
+    }  
+}
---
+++ BookStoreDashboard/Models/BookViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreDashboard.Models
+{
+    public class BookViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+
+        public string Description { get; set; }
+        [Required]
+
+        public string Author { get; set; }
+        [Required]
+
+        public string Genre { get; set; }
+        [Required]
+
+        public int Quantity { get; set; }
+        [Required]
+
+        public decimal Price { get; set; }
+    }
+}
